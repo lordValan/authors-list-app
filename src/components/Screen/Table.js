6bd6ec2e06a98204dfc2c6bd41de48bd70062e95
Danil Fomin <fominitstep@gmail.com>
2019-02-13 14:35:34 +0200

    Table component styles commit

--- src/components/Screen/Table.js
+++ src/components/Screen/Table.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import randomColor from 'randomcolor';
 
 export default class Table extends Component {
     constructor(props) {
@@ -22,42 +23,45 @@ export default class Table extends Component {
         const { firstIndex, authors } = this.props;
 
         return (
-            <table className={`authors-table`}>
-                <tbody>
-                    {authors.map((author, index) => {
-                        let pubsClass = author.count_pub > 0 ? `pubs` : `pubs null`;
-                        return <tr key={`author_${index}`} className={`author-row`}>
-                            <td className={`author-cell cell-num`}>
-                                <span className={`num`}>
-                                    {firstIndex + index + 1}
-                                </span>
-                            </td>
-                            <td className={`author-cell cell-letter`}>
-                                <span className={`f-letter`}>
-                                    {author.name.charAt(0)}
-                                </span>
-                            </td>
-                            <td className={`author-cell cell-name-pub`}>
-                                <span className={`name`}>
-                                    {author.name}
-                                </span>
-                                <br />
-                                <span className={`${pubsClass}`}>
-                                    {`${author.count_pub} публ.`}
-                                </span>
-                            </td>
-                            <td className={`author-cell cell-ach`}>
-                                {this.drawAchievement(author.pageviews)}
-                            </td>
-                            <td className={`author-cell cell-views`}>
-                                <span className={`pageviews`}>
-                                    {author.pageviews}
-                                </span>
-                            </td>
-                        </tr>
-                    })}
-                </tbody>
-            </table>
+            <div className={`authors-table`}>
+                {authors.map((author, index) => {
+                    let pubsClass = author.count_pub > 0 ? `pubs` : `pubs null`;
+                    
+                    return <div key={`author_${index}`} className={`author-row`}>
+                        <div className={`author-cell cell-num`}>
+                            <span className={`num`}>
+                                {firstIndex + index + 1}
+                            </span>
+                        </div>
+                        <div className={`author-cell cell-letter`}>
+                            <span className={`f-letter`} style={{
+                                backgroundColor: randomColor({
+                                    luminosity: 'bright',
+                                    format: 'rgb'
+                                })
+                            }}>
+                                {author.name.charAt(0).toUpperCase()}
+                            </span>
+                        </div>
+                        <div className={`author-cell cell-name-pub`}>
+                            <span className={`name`}>
+                                {author.name}
+                            </span>
+                            <span className={`${pubsClass}`}>
+                                {`${author.count_pub} публ.`}
+                            </span>
+                        </div>
+                        <div className={`author-cell cell-ach`}>
+                            {this.drawAchievement(author.pageviews)}
+                        </div>
+                        <div className={`author-cell cell-views`}>
+                            <span className={`pageviews`}>
+                                {author.pageviews}
+                            </span>
+                        </div>
+                    </div>
+                })}
+            </div>
         );
     }
 }
